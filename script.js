21838a09fec78c196d7ff42d66164de0933c040c
zeyadkhalaf <109981053+zeyadkhalaf@users.noreply.github.com>
2022-08-18 18:02:01 +0300

    cw7 is done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function BMI(weight, height) {
+    let result = weight / (height * height) 
+    return result;
+}
+console.log(BMI(70, 1.7));
 
+function Status(BMI) {
+    if (bmi < 18.5) {
+        return "لديك نقص في الوزن";
+    } else if (BMI >= 18.5 && BMI < 25){
+        return "وزنك صحي";
+    } else {
+        return "لديك زيادة في الوزن";
+    }
+}
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let bmi = BMI(weight, height);
+    let desc = Status(bmi);
+    let div = document.getElementById("result");
+    div.innerText = bmi + " == " + desc;
+}
